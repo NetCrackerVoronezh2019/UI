aff85c16cbcfd5e6d0baf7668024ca6248ec54b4
Armen <armentovmasyan02@gmail.com>
2020-05-06 22:06:49 +0300

    jdska

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/help-student'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/help-student/index.html'));
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
