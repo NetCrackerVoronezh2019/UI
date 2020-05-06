5393a9b720177a05982406f301bb4d9350cc1c17
Armen <armentovmasyan02@gmail.com>
2020-05-06 22:02:31 +0300

    update

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
  
 const app = express();
  
-app.use(express.static(__dirname + '/dist/help-student'));
+app.use(express.static(__dirname + '/help-student'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/help-student/index.html'));
+res.sendFile(path.join(__dirname+'/help-student/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
