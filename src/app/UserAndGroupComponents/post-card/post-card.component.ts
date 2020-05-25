15be5a0a85d21672dad6e60f931cf20b0b769d92
Igor Pereslavtsev <Ig0r33@mail.ru>
2020-05-25 12:50:22 +0300

    fix comments

--- src/app/UserAndGroupComponents/post-card/post-card.component.ts
+++ src/app/UserAndGroupComponents/post-card/post-card.component.ts
@@ -75,7 +75,7 @@ export class PostCardComponent implements OnInit {
   }
 
   sendComment() {
-    if (this.ps.getCommentCreateForm().controls['text'].invalid) {
+    if (!this.ps.getCommentCreateForm().controls['text'].invalid) {
       this.ps.sendComment(this.user,this.post.postId).subscribe(data => {
         this.getComments();
         this.createCommentVisible = false;
